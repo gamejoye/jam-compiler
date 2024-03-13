7066073096bf60fb1f5a91118d14a7422d8c9ed5
gamejoye <gamejoye@gmail.com>
2024-03-13 18:20:53 +0800

    test(lexer): unit test lexer

---
+++ tests/lexer_tests.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <gtest/gtest.h>
+#include <fstream>
+#include "utilities.h"
+
+extern int yylex(void);
+extern int yy_scan_string(const char *str);
+extern FILE *yyin;
+
+static std::string examplesDir = "tests/lexer_test_examples/";
+
+TEST(LexerGoodTest, CanRecognizeTokens1) {
+  yyin = fopen((examplesDir + "good_test1.jam").c_str(), "r");
+  std::ifstream targetFile(examplesDir + "good_test1.expected");
+  int token;
+  std::string expected;
+  while ((token = yylex()) != 0 && std::getline(targetFile, expected)) {
+    ASSERT_EQ(expected, mapToToken(token));
+  }
+
+  if(token != 0) {
+    ASSERT_TRUE(false);
+  }
+
+  fclose(yyin);
+}
+
+TEST(LexerGoodTest, CanRecognizeTokens2) {
+  yyin = fopen((examplesDir + "good_test2.jam").c_str(), "r");
+  std::ifstream targetFile(examplesDir + "good_test2.expected");
+  int token;
+  std::string expected;
+  while ((token = yylex()) != 0 && std::getline(targetFile, expected)) {
+    ASSERT_EQ(expected, mapToToken(token));
+  }
+
+  if(token != 0) {
+    ASSERT_TRUE(false);
+  }
+
+  fclose(yyin);
+}
+
+TEST(LexerGoodTest, CanRecognizeTokens3) {
+  yyin = fopen((examplesDir + "good_test3.jam").c_str(), "r");
+  std::ifstream targetFile(examplesDir + "good_test3.expected");
+  int token;
+  std::string expected;
+  while ((token = yylex()) != 0 && std::getline(targetFile, expected)) {
+    ASSERT_EQ(expected, mapToToken(token));
+  }
+
+  if(token != 0) {
+    ASSERT_TRUE(false);
+  }
+
+  fclose(yyin);
+}
